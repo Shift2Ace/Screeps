257972f3a6a630b8cb77c18dbb13d5a75870d83b
Shift2Ace <121743683+Shift2Ace@users.noreply.github.com>
2023-07-01 16:39:31 +0800

    Update harvester.js

--- modules/role/harvester.js
+++ modules/role/harvester.js
@@ -11,8 +11,10 @@ var roleHarvester = {
         else {//else creep have no space (full)
             var targets = creep.room.find(FIND_STRUCTURES, {  //get target(STRUCTURES)
                     filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) && //if target is extension or spawn
-                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;//and it has space
+                        return (structure.structureType == STRUCTURE_EXTENSION || //if target is extension or spawn or town
+								structure.structureType == STRUCTURE_SPAWN ||
+			        			structure.structureType == STRUCTURE_TOWER)) && 
+                            	structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;//and it has space
                     }
             });
             if(targets.length) {	//if there is target
