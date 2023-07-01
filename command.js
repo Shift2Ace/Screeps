5863149aec26d4e54fe50d03404a6b07a0bc3397
Shift2Ace <121743683+Shift2Ace@users.noreply.github.com>
2023-07-01 19:44:02 +0800

    Update command.js

--- command.js
+++ command.js
@@ -2,3 +2,4 @@ Game.spawns['Spawn1'].spawnCreep([<body>], '<Name>',{memory: {role: '<role>'}});
 Game.creeps['<name of screep>'].suicide(); //kill the creep
 Game.spawns['Spawn1'].room.controller.activateSafeMode(); //active safe mode
 Game.spawns['Spawn1'].room.createConstructionSite( 23, 22, STRUCTURE_TOWER ); //create tower
+Room.createConstructionSite(x, y, STRUCTURE_EXTENSION) //create extension
